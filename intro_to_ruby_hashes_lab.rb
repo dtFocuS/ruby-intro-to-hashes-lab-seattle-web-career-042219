02bdbe0a70d7bf9dad45a0037b5c541ab66abbb1
Danny Tseng <dtfocus1025@gmail.com>
2019-04-09 01:40:51 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,14 +7,14 @@ def actor
 end
 
 def monopoly
-	new_hash = {:railroads => {}}
+	monopoly = {:railroads => {}}
 
 end
 
 def monopoly_with_second_tier
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-  second_tier = monopoly
-	second_tier[:railroads][:pieces] = 4
+  monopoly = {:railroads => {}}
+	monopoly[:railroads][:pieces] = 4
 	
 	}
 
